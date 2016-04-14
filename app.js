77c7e24d47b7fdb1ab599a1817a521bf873581d1
jcarlosmolina <jcarlosmolina@gmail.com>
2016-04-14 07:12:04 +0200

    Factor out list of posts to service

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl', [
-	'$scope',
-	function($scope){
-		$scope.test = 'Hello, world!';
-		$scope.posts = [
+app.factory('posts', function () {
+	var o = {
+		posts: [
 			{title: 'post 1', upvotes: 5}, 
 			{title: 'post 2', upvotes: 2},
 			{title: 'post 3', upvotes: 15},
 			{title: 'post 4', upvotes: 9},
 			{title: 'post 5', upvotes: 4}
-		];
+		]
+	};
+	return o;
+});
+
+app.controller('MainCtrl', [
+	'$scope', 'posts',
+	function($scope, posts){
+		$scope.test = 'Hello, world!';
+		$scope.posts = posts.posts;
 
 		var addPostPreconditions = function(){
 			return ($scope.title && $scope.title !== '');
